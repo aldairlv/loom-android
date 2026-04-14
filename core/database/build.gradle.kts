e0011cd864ee0b17d1a3fe2cfb34e522069e0fe4
Aldair López <aldair.orlando.lopez@estudiantat.upc.edu>
2026-04-14 02:21:46 +0200

    feat: enable schema export and configure ksp location

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -19,8 +19,12 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-    }
 
+
+    }
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
