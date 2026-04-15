ac86ae0c5b6d79e9ea5f97214a9b87c078af45ad
Aldair López <aldair.orlando.lopez@estudiantat.upc.edu>
2026-04-15 23:01:38 +0200

    feat: define navigation entry provider for ForYou feature

---
+++ feature/foryou/impl/src/main/java/com/loom/feature/foryou/impl/navigation/ForYouEntryProvider.kt
@@ -0,0 +1,15 @@
+package com.loom.feature.foryou.impl.navigation
+
+
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+import com.loom.core.navigation.Navigator
+
+import com.loom.feature.foryou.api.navigation.ForYouNavKey
+
+
+fun EntryProviderScope<NavKey>.forYouEntry(navigator: Navigator) {
+    entry<ForYouNavKey> {
+        // TODO: Aquí cargamos la pantalla de For You
+    }
+}
