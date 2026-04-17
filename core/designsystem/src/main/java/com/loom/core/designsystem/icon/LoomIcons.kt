30279a8edf8563b93f89e09a96860e9528fe0daa
Aldair López <aldair.orlando.lopez@estudiantat.upc.edu>
2026-04-17 18:38:22 +0200

    feat: add social and navigation icons to LoomIcons

---
+++ core/designsystem/src/main/java/com/loom/core/designsystem/icon/LoomIcons.kt
@@ -0,0 +1,38 @@
+package com.loom.core.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.rounded.Groups
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+/**
+ * Loom icons. Material icons are [ImageVector]s.
+ */
+object LoomIcons {
+    // Home
+    val Home = Icons.Rounded.Home
+    val HomeBorder = Icons.Outlined.Home
+
+    // Search
+    val Search = Icons.Rounded.Search
+    val SearchBorder = Icons.Outlined.Search
+
+    // Events
+    val Events = Icons.Rounded.Groups
+    val EventsBorder = Icons.Rounded.Groups
+
+    // Profile
+    val Person = Icons.Rounded.Person
+    val PersonBorder = Icons.Outlined.Person
+
+    // Notifications
+    val Notifications = Icons.Rounded.Notifications
+    val NotificationsBorder = Icons.Outlined.Notifications
+}
