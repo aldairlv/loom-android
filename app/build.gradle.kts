c789950a6a928424c77f00fe0db4fd7922859a64
Aldair López <aldair.orlando.lopez@estudiantat.upc.edu>
2026-04-17 19:19:30 +0200

    feat: implement top level navigation destinations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(project(":core:designsystem"))
+    implementation(project(":feature:foryou:api"))
 }
