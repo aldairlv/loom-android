10da226ec82315f6d779bf38a2c25fb09fd0b380
Aldair López <aldair.orlando.lopez@estudiantat.upc.edu>
2026-04-18 17:48:25 +0200

    feat: define UserDataRepository interface

---
+++ core/data/src/main/java/com/loom/core/data/repository/UserDataRepository.kt
@@ -0,0 +1,35 @@
+package com.loom.core.data.repository
+
+import com.loom.core.model.data.DarkThemeConfig
+import com.loom.core.model.data.ThemeBrand
+import com.loom.core.model.data.UserData
+import kotlinx.coroutines.flow.Flow
+
+
+interface UserDataRepository {
+
+    /**
+     * Stream of [UserData]
+     */
+    val userData: Flow<UserData>
+
+    /**
+     * Sets the desired theme brand.
+     */
+    suspend fun setThemeBrand(themeBrand: ThemeBrand)
+
+    /**
+     * Sets the desired dark theme config.
+     */
+    suspend fun setDarkThemeConfig(darkThemeConfig: DarkThemeConfig)
+
+    /**
+     * Sets the preferred dynamic color config.
+     */
+    suspend fun setDynamicColorPreference(useDynamicColor: Boolean)
+
+    /**
+     * Sets whether the user has completed the onboarding process.
+     */
+    suspend fun setShouldHideOnboarding(shouldHideOnboarding: Boolean)
+}
