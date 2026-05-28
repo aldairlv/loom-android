3824b7ef083ae40480c212c9efdad4d778653bcd
Aldair López <aldair.orlando.lopez@estudiantat.upc.edu>
2026-05-28 09:03:47 +0200

    feat: implement main profile screen and viewmodel

--- feature/profile/impl/src/main/java/com/loom/feature/profile/impl/navigation/ProfileEntryProvider.kt
+++ feature/profile/impl/src/main/java/com/loom/feature/profile/impl/navigation/ProfileEntryProvider.kt
@@ -3,12 +3,15 @@ package com.loom.feature.profile.impl.navigation
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.loom.core.navigation.Navigator
+import com.loom.feature.posteditor.api.navigation.CreatePostNavKey
 import com.loom.feature.profile.api.navigation.ProfileNavKey
 import com.loom.feature.profile.impl.ProfileScreen
 fun EntryProviderScope<NavKey>.profileEntry(navigator: Navigator) {
     entry<ProfileNavKey> {
         ProfileScreen(
-
+            onCreateClick = {
+                navigator.navigate(CreatePostNavKey)
+            }
         )
     }
 }
